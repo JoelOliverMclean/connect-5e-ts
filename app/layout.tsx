e73f020398e661f80640d83c7ba933fb31a5f2ad
Joel Oliver Mclean <joelolivermclean@gmail.com>
2025-03-14 17:01:54 +0000

    Scrolling layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,5 @@
-import { type Metadata } from "next";
 import { Cinzel } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 import Link from "next/link";
 import { dark } from "@clerk/themes";
@@ -35,20 +27,24 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={`${cinzelSerif.className} antialiased`}>
-          <header className="flex justify-between items-center p-4 gap-4 h-16 bg-red-900">
-            <Link className="text-2xl font-bold" href={"/"}>
-              Connect5e
-            </Link>
-            <div className="flex items-center gap-4">
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </div>
-          </header>
-          <SignedOut>
-            <LandingContent />
-          </SignedOut>
-          <SignedIn>{children}</SignedIn>
+          <div className="h-screen bg-gradient-to-b to-stone-800 from-stone-950 flex flex-col safe-area">
+            <header className="sticky-top flex justify-between items-center p-4 gap-4 h-16 bg-red-900">
+              <Link className="text-2xl font-bold" href={"/"}>
+                Connect5e
+              </Link>
+              <div className="flex items-center gap-4">
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+              </div>
+            </header>
+            <main className="container flex-grow mx-auto overflow-y-auto">
+              <SignedOut>
+                <LandingContent />
+              </SignedOut>
+              <SignedIn>{children}</SignedIn>
+            </main>
+          </div>
         </body>
       </html>
     </ClerkProvider>
