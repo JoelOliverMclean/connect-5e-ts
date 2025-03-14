9753380b4f026ce89eed89da3e31ede8f38fdbef
Joel Oliver Mclean <joelolivermclean@gmail.com>
2025-03-14 17:01:58 +0000

    Home page shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  const myCharacters = (
+    <div className="flex flex-col gap-2">
+      <h5>My Characters</h5>
+      <div className="opacity-50">None yet</div>
+    </div>
+  );
+  const myCampaigns = (
+    <div className="flex flex-col gap-2">
+      <h5>My Campaigns</h5>
+      <div className="opacity-50">None yet</div>
+    </div>
+  );
+
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col gap-4 p-4">
+      <h4 className="">Welcome back, {user?.firstName}</h4>
+      <hr />
+      {myCharacters}
+      {myCampaigns}
     </div>
   );
 }
